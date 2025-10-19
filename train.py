88d0306afb6b2bea360841b5c2aed7bece034bad
TQ Zhang <tianqiz04@gmail.com>
2025-10-19 15:13:41 -0700

    fix: metric mismatch

--- train.py
+++ train.py
@@ -250,7 +250,7 @@ def main(cfg: BabyLMConfig):
         is not None,  # NOTE: This is to ensure that the curriculum is not broken on the last batch
         remove_unused_columns=False,
         load_best_model_at_end=True,
-        metric_for_best_model="eval_perplexity_mean",
+        metric_for_best_model="perplexity",
         greater_is_better=False,  # smaller perplexity is better
         ddp_find_unused_parameters=False,
         ddp_timeout=28800,  # 8 hours (default is 30 minutes)
